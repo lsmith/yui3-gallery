9c0d2d83cdc000845b24b2cc5c9d5a1002f230fc
Luke Smith <lsmith@yahoo-inc.com>
2012-01-04 17:14:27 -0800

    -infinite loop in getColumn. +datatable-mutation

--- src/gallery-datatable-350-preview/js/datatable.js
+++ src/gallery-datatable-350-preview/js/datatable.js
@@ -381,13 +381,21 @@ Y.mix(Table.prototype, {
             while (stack.length) {
                 entry = stack[stack.length - 1];
                 cols  = entry[0];
-                for (i = entry[1]; i >= 0; --i) {
+                for (i = entry[1], len = cols.length; i < len; ++i) {
                     col = cols[i];
                     // Only need to check against name because the initial
                     // col = get('columns.' + name) would get it from the key map
                     if (col.name === name) {
                         return col;
+                    } else if (col.children) {
+                        entry[1] = i + 1;
+                        stack.push([col.children, 0]);
+                        break;
                     }
+                }
+
+                if (i > len) {
+                    stack.pop();
                 }
             }
         }
@@ -575,11 +583,10 @@ Y.mix(Table.prototype, {
     @protected
     **/
     _getColumns: function (columns, name) {
-        // name will be 'columns' or 'columns.foo'. Trim to the dot.
-        // TODO: support name as an index or (row,column) index pair
-        name = name.slice(8);
-
-        return (name) ? this._columnMap[name] : columns;
+        // Workaround for an attribute oddity (ticket #2529254)
+        // getter is expected to return an object if get('columns.foo') is called.
+        // Note 'columns.' is 8 characters
+        return name.length > 8 ? this._columnMap : columns;
     },
 
     /**
@@ -1181,6 +1188,7 @@ Y.mix(Table.prototype, {
     }
 });
 }, '@VERSION@', { requires: ['model-list'] });
+
 YUI.add('gallery-datatable-350-preview-head', function (Y) {
 /**
 View class responsible for rendering the `<thead>` section of a table. Used as
@@ -1679,6 +1687,7 @@ Y.namespace('DataTable').HeaderView = Y.Base.create('tableHeader', Y.View, [], {
     }
 });
 }, '@VERSION@', { requires: ['view', 'gallery-datatable-350-preview-core'] });
+
 YUI.add('gallery-datatable-350-preview-body', function (Y) {
 /**
 View class responsible for rendering the `<tbody>` section of a table. Used as
@@ -2448,7 +2457,8 @@ Y.namespace('DataTable').BodyView = Y.Base.create('tableBody', Y.View, [], {
     //_rowTemplate: null
 });
 }, '@VERSION@', { requires: ['view', 'gallery-datatable-350-preview-core'] });
-Y.use('gallery-datatable-350-preview-core', 'gallery-datatable-350-preview-head', 'gallery-datatable-350-preview-body');
+
+YUI.add('gallery-datatable-350-preview-base', function (Y) {
 /**
 A Widget for displaying tabular data.  The base implementation of DataTable
 provides the ability to dynamically generate an HTML table from a set of column
@@ -2709,3 +2719,263 @@ Y.DataTable.Base = Y.Base.create('datatable', Y.Widget, [Y.DataTable.Core],
 Y.DataTable = Y.mix(
     Y.Base.create('datatable', Y.DataTable.Base, []), // Create the class
     Y.DataTable); // Migrate static and namespaced classes
+}, '@VERSION@', { requires: ['model-list', 'view', 'base-build', 'widget', 'escape', 'gallery-datatable-350-preview-core'] });
+
+YUI.add('gallery-datatable-350-preview-mutable', function (Y) {
+var toArray = Y.Array,
+    YLang   = Y.Lang,
+    isString = YLang.isString,
+    isArray  = YLang.isArray,
+    isObject = YLang.isObject,
+    isNumber = YLang.isNumber,
+    arrayIndex = Y.Array.indexOf,
+    Mutable;
+
+/**
+Adds mutation convenience methods to `Y.DataTable` (or other built class).
+
+@module datatable-mutation
+**/
+
+Y.namespace('DataTable').Mutable = Mutable = function () {};
+
+Y.mix(Mutable.prototype, {
+    /**
+    Adds the column configuration to the DataTable's `columns` configuration.
+    If the `index` parameter is supplied, it is injected at that index.  If the
+    table has nested headers, inject a subcolumn by passing an array of indexes
+    to identify the new column's final location.
+
+    The `index` parameter is required if adding a nested column.
+
+    This method is a convienience method for fetching the DataTable's `columns`
+    attribute, updating it, and calling 
+    `table.set('columns', _updatedColumnsDefs_)`
+
+    @example
+    // Becomes last column
+    table.addColumn('name');
+
+    // Inserted after the current second column, moving the current third column
+    // to index 4
+    table.addColumn({ key: 'price', formatter: currencyFormatter }, 2 );
+
+    // Insert a new column in a set of headers three rows deep.  The index array
+    // translates to
+    // [ 2, --  in the third column's children
+    //   1, --  in the second child's children
+    //   3 ] -- as the fourth child column
+    table.addColumn({ key: 'age', sortable: true }, [ 2, 1, 3 ]);
+    **/
+    addColumn: function (config, index) {
+        if (isString(config)) {
+            config = { key: config };
+        }
+
+        if (config) {
+            if (arguments.length < 2 || (!isNumber(index) && !isArray(index))) {
+                index = this.get('columns').length;
+            }
+
+            this.fire('addColumn', {
+                column: config,
+                index: index
+            });
+        }
+        return this;
+    },
+
+    modifyColumn: function (name, config) {
+        if (isString(config)) {
+            config = { key: config };
+        }
+
+        if (isObject(config)) {
+            this.fire('modifyColumn', {
+                column: name,
+                newColumnDef: config
+            });
+        }
+
+        return this;
+    },
+
+    moveColumn: function (name, index) {
+        if (name && (isNumber(index) || isArray(index))) {
+            this.fire('moveColumn', {
+                column: name,
+                index: index
+            });
+        }
+
+        return this;
+    },
+
+    removeColumn: function (name) {
+        if (name) {
+            this.fire('removeColumn', {
+                column: name
+            });
+        }
+
+        return this;
+    },
+
+    addRow: function () {
+        if (this.data) {
+            this.data.add.apply(this.data, arguments);
+        }
+
+        return this;
+    },
+
+    removeRow: function (id) {
+        var modelList = this.data,
+            model;
+
+        // TODO: support removing via DOM element. This should be relayed to View
+        if (isObject(id) && id instanceof this.get('recordType')) {
+            model = id;
+        } else if (modelList && id !== undefined) {
+            model = modelList.getById(id) ||
+                    modelList.getByClientId(id) ||
+                    modelList.item(id);
+        }
+
+        if (model) {
+            modelList.remove.apply(modelList,
+                [model].concat(toArray(arguments, 1, true)));
+        }
+
+        return this;
+    },
+
+    modifyRow: function (id, data) {
+        var modelList = this.data,
+            model;
+
+        if (isObject(id) && id instanceof this.get('recordType')) {
+            model = id;
+        } else if (modelList && id !== undefined) {
+            model = modelList.getById(id) ||
+                    modelList.getByClientId(id) ||
+                    modelList.item(id);
+        }
+
+        if (model && isObject(data)) {
+            model.setAttrs.apply(model, toArray(arguments, 1, true));
+        }
+
+        return this;
+    },
+
+    reset: function (data) {
+        if (this.data) {
+            this.data.reset.apply(this.data, arguments);
+        }
+
+        return this;
+    },
+
+    // --------------------------------------------------------------------------
+    // Protected properties and methods
+    // --------------------------------------------------------------------------
+
+    _defAddColumnFn: function (e) {
+        var index   = toArray(e.index),
+            columns = this.get('columns'),
+            cols    = columns,
+            i, len;
+
+        for (i = 0, len = index.length - 1; cols && i < len; ++i) {
+            cols = cols[index[i]] && cols[index[i]].children;
+        }
+
+        if (cols) {
+            cols.splice(index[i], 0, e.column);
+
+            this.set('columns', columns, { originEvent: e });
+        } else { Y.log('addColumn index not findable', 'warn', 'datatable');
+        }
+    },
+
+    _defModifyColumnFn: function (e) {
+        var columns = this.get('columns'),
+            column  = this.getColumn(e.column);
+
+        if (column) {
+            Y.mix(column, e.newColumnDef, true);
+            
+            this.set('columns', columns, { originEvent: e });
+        } else { Y.log('Could not locate column index to modify column', 'warn', 'datatable');
+        }
+    },
+
+    _defMoveColumnFn: function (e) {
+        var columns = this.get('columns'),
+            column  = this.getColumn(e.column),
+            toIndex = toArray(e.index),
+            fromCols, fromIndex, toCols, i, len;
+
+        if (column) {
+            fromCols  = column.parent ? column.parent.children : columns;
+            fromIndex = arrayIndex(fromCols, column);
+
+            if (fromIndex > -1) {
+                toCols = columns;
+
+                for (i = 0, len = toIndex.length - 1; toCols && i < len; ++i) {
+                    toCols = toCols[i] && toCols[i].children;
+                }
+
+                if (toCols) {
+                    fromCols.splice(fromIndex, 1);
+                    toCols.splice(toIndex[i], 1, column);
+
+                    this.set('columns', columns, { originEvent: e });
+                } else { Y.log('Column [' + e.column + '] could not be moved. Destination index invalid for moveColumn', 'warn', 'datatable');
+                }
+            }
+        } else { Y.log('Column [' + e.column + '] not found for moveColumn', 'warn', 'datatable');
+        }
+    },
+
+    _defRemoveColumnFn: function (e) {
+        var columns = this.get('columns'),
+            column  = this.getColumn(e.column),
+            cols, index;
+
+        if (column) {
+            cols = column.parent ? column.parent.children : columns;
+            index = Y.Array.indexOf(cols, column);
+
+            if (index > -1) {
+                cols.splice(index, 1);
+
+                this.set('columns', columns, { originEvent: e });
+            }
+        } else { Y.log('Could not locate column [' + e.column + '] for removeColumn', 'warn', 'datatable');
+        }
+    },
+
+    initializer: function () {
+        this.publish({
+            addColumn:    { defaultFn: this._defAddColumnFn },
+            removeColumn: { defaultFn: this._defRemoveColumnFn },
+            moveColumn:   { defaultFn: this._defMoveColumnFn },
+            modifyColumn: { defaultFn: this._defModifyColumnFn }
+        });
+    }
+});
+
+Mutable.prototype.addRows = Mutable.prototype.addRow;
+
+// Add feature APIs to public Y.DataTable class
+Y.Base.mix(Y.DataTable, [Mutable]);
+}, '@VERSION@', { requires: ['gallery-datatable-350-preview-base'] });
+
+Y.use('gallery-datatable-350-preview-core', 
+      'gallery-datatable-350-preview-head', 
+      'gallery-datatable-350-preview-body', 
+      'gallery-datatable-350-preview-base', 
+      'gallery-datatable-350-preview-mutable');
